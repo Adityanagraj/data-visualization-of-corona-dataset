f605964f5254577cad13c59956b327021a285fab
Adityanagraj <42292430+Adityanagraj@users.noreply.github.com>
2020-03-25 22:14:13 +0530

    Add files via upload

---
+++ web4.py
@@ -0,0 +1,27 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+f=csv.writer(open('covid.csv','w'))
+f.writerow(['country_other','total_cases','new_cases','total_death','new_death','total_recovered','active_cases','serious','tot_cases','total_death'])
+url='https://www.worldometers.info/coronavirus/'
+page=requests.get(url)
+page=BeautifulSoup(page.text,'html.parser')
+content=page.find(class_='main_table_countries_div')
+content_data=content.find_all('tr')
+for data in content_data:
+	country_other=(data.contents[1].text)
+	total_cases=(data.contents[3].text)
+	new_cases=(data.contents[5].text)
+	total_death=(data.contents[7].text)
+	new_death=(data.contents[9].text)
+	total_recovered=(data.contents[11].text)
+	active_cases=(data.contents[13].text)
+	serious=(data.contents[15].text)
+	tot_cases=(data.contents[17].text)
+	total_death=(data.contents[19].text)
+	f.writerow([country_other,total_cases,new_cases,total_death,new_death,total_recovered,active_cases,serious,tot_cases,total_death])
+
+
+
+
